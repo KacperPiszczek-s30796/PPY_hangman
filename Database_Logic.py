b765c9d4c69b4fb0e4b69f8d25d77824445a121b
Twoje Imi i Nazwisko <Twj email powizany z GitHubem>
2025-05-17 21:27:10 +0200

    player basic encryption

--- Database_Logic.py
+++ Database_Logic.py
@@ -2,6 +2,34 @@ import random
 import os
 
 
+def encrypt(text: str) -> str:
+    result = []
+    for c in text:
+        if 'A' <= c <= 'Z':
+            result.append(chr((ord(c) - ord('A') + 1) % 26 + ord('A')))
+        elif 'a' <= c <= 'z':
+            result.append(chr((ord(c) - ord('a') + 1) % 26 + ord('a')))
+        elif '0' <= c <= '9':
+            result.append(chr((ord(c) - ord('0') + 1) % 10 + ord('0')))
+        else:
+            result.append(c)
+    return ''.join(result)
+
+
+def decrypt(text: str) -> str:
+    result = []
+    for c in text:
+        if 'A' <= c <= 'Z':
+            result.append(chr((ord(c) - ord('A') - 1) % 26 + ord('A')))
+        elif 'a' <= c <= 'z':
+            result.append(chr((ord(c) - ord('a') - 1) % 26 + ord('a')))
+        elif '0' <= c <= '9':
+            result.append(chr((ord(c) - ord('0') - 1) % 10 + ord('0')))
+        else:
+            result.append(c)
+    return ''.join(result)
+
+
 def get_random_word() -> str:
     with open("Categories.txt", "r") as file:
         category = file.read()
@@ -20,6 +48,8 @@ def get_random_word() -> str:
 
 
 def register(name: str, password: str) -> bool:
+    name = encrypt(name)
+    password = encrypt(password)
     path = "players/"+name+".txt"
     if os.path.exists(path):
         return False
@@ -30,6 +60,8 @@ def register(name: str, password: str) -> bool:
 
 
 def login(name: str, password: str) -> bool:
+    name = encrypt(name)
+    password = encrypt(password)
     path = "players/" + name + ".txt"
     if os.path.exists(path):
         with open(path, "r") as file:
@@ -44,16 +76,19 @@ def login(name: str, password: str) -> bool:
 
 
 def read_statistics(name: str) -> str:
+    name = encrypt(name)
     path = "players/" + name + ".txt"
     with open(path, "r") as file:
         text = file.read()
     statistics = text.strip().split("\n", 1)
     if len(statistics) == 1:
         return "0\n0\n0\n0"
-    return statistics[1]
+    return decrypt(statistics[1])
 
 
 def write_statistics(name: str, statistics: str) -> bool:
+    name = encrypt(name)
+    statistics = encrypt(statistics)
     path = "players/" + name + ".txt"
     with open(path, "r") as file:
         lines = file.readlines()
